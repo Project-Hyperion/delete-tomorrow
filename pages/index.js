d3c6290255fd97de56c234cc997c23d709c1ad34
Ultra Bytes <ultrabytes@outlook.fr>
2021-02-07 19:27:17 +0100

    hi

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Amoy is back at it again!</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Amoy is back at it again!</a>
         </h1>
 
         <p className={styles.description}>
